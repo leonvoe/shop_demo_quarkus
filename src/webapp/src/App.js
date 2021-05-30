af60b6fcd9fafc0dbd05d1f49fec26b0d60ded86
Leon Völlinger <leonvoellinger@web.de>
2021-05-31 00:39:55 +0200

    Deleted test files, starting from scratch again

--- src/webapp/src/App.js
+++ src/webapp/src/App.js
@@ -1,26 +1,8 @@
 import logo from "./logo.svg";
 import "./App.css";
-import Navbar from "./components/Navbar";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import Home from "./pages/Home";
-import Customers from "./pages/Customers";
-import Orders from "./pages/Orders";
-import Articles from "./pages/Articles";
 
 function App() {
-  return (
-    <div className="App">
-      <Router>
-        <Navbar />
-        <Switch>
-          <Route path="/" exact component={Home} />
-          <Route path="/customers" component={Customers} />
-          <Route path="/articles" component={Articles} />
-          <Route path="/orders" component={Orders} />
-        </Switch>
-      </Router>
-    </div>
-  );
+  return <div className="App"></div>;
 }
 
 export default App;
